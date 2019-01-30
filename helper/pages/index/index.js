d439648cf234528eceb79eb6c3c172137ce8f39e
Lee <545360100@qq.com>
2019-01-30 15:17:00 +0800

    复制功能完成

--- helper/pages/index/index.js
+++ helper/pages/index/index.js
@@ -1,55 +1,100 @@
 //index.js
 //获取应用实例
-import {translate} from '../../utils/api.js'
+// import {translate} from '../../utils/api.js'
+import {
+  translate
+} from '../../utils/youdao-api.js'
 const app = getApp()
 
 Page({
-  data:{
-    query:'',
-    hideClearIcon:true,
-    result:[],
-    curLang:{}
+  data: {
+    query: '',
+    hideClearIcon: true,
+    result: [],
+    curLang: {},
+    hideCopyBtn: true,
   },
-  onLoad:function(options){
+  onLoad: function(options) {
     console.log('loaded..')
     console.log(options)
-    if(options.query){
-      this.setData({query:options.query})
+    if (options.query) {
+      this.setData({
+        query: options.query
+      })
     }
   },
-  onShow:function(){
-    if(this.data.curLang.lang!==app.globalData.curLang.lang){
-      this.setData({curLang:app.globalData.curLang})
+  onShow: function() {
+    if (this.data.curLang.lang !== app.globalData.curLang.lang) {
+      this.setData({
+        curLang: app.globalData.curLang
+      })
       this.onConfirm()
     }
   },
-  onInput:function(e){
-    this.setData({'query':e.detail.value})
-    if(this.data.query.length>0){
-      this.setData({'hideClearIcon':false})
-    }else{
-      this.setData({'hideClearIcon':true})
+  onInput: function(e) {
+    this.setData({
+      'query': e.detail.value
+    })
+    if (this.data.query.length > 0) {
+      this.setData({
+        'hideClearIcon': false
+      })
+    } else {
+      this.setData({
+        'hideClearIcon': true
+      })
     }
     console.log('focus')
   },
-  onTapClose:function(){
-    this.setData({'hideClearIcon':true,'query':''})
+  onTapClose: function() {
+    this.setData({
+      'hideClearIcon': true,
+      'query': '',
+      'result':'',
+      'hideCopyBtn':true
+    })
   },
-  onConfirm:function(){
-    if(!this.data.query) return
-    translate(this.data.query, { from: 'auto', to: this.data.curLang.lang}).then(res=>{
-      this.setData({ 'result': res.trans_result})
+  onTapCopy: function() {
+    if (!this.data.query) {
+      wx.showToast({
+        title: '没有结果',
+      })
+      return
+    }
+    wx.setClipboardData({
+      data: String(this.data.result),
+      success: res => {
+        wx.showToast({
+          title: '复制成功',
+        })
+      }
+    })
+  },
+  onConfirm: function() {
+    if (!this.data.query) return
+    translate(this.data.query, {
+      from: 'auto',
+      to: this.data.curLang.lang
+    }).then(res => {
+      console.log("indexres", res)
+      this.setData({
+        'result': res.translation,
+        'hideCopyBtn': false
+      })
 
-      let history= wx.getStorageSync('history')||[]
+      let history = wx.getStorageSync('history') || []
       console.log(history)
-      history.unshift({query:this.data.query, result:res.trans_result[0].dst})
-      history.length = history.length>10?10:history.length
-      try{
-        wx.setStorageSync('history',history )
-      }catch(e){
+      history.unshift({
+        query: this.data.query,
+        result: res.translation[0]
+      })
+      history.length = history.length > 10 ? 10 : history.length
+      try {
+        wx.setStorageSync('history', history)
+      } catch (e) {
         console.log(e)
       }
-      
+
     })
   }
 })
---
+++ helper/utils/youdao-api.js
@@ -0,0 +1,50 @@
+import md5 from './md5.min.js'
+
+const appid = '776c13e8936efb4f'
+const key = 'H95eg7NJMmtxJXL2Dbwf6EDEvCJsS3Mu'
+
+// 百度api http://api.fanyi.baidu.com/api/trans/product/apidoc
+// ={from:'auto',to:'en'}默认参数语法 如果只传from或者to则使用{ from = 'auto', to = 'en' }
+// 目标语言不能为auto
+function translate(q, { from = 'auto', to = 'zh' } = { from: 'auto', to: 'zh' }) {
+  return new Promise((resolve, reject) => {
+    let salt = Date.now()
+    let sign = md5(`${appid}${q}${salt}${key}`)
+    if(to==='en'){
+      to="EN"
+    }
+    wx.request({
+      url: 'https://openapi.youdao.com/api',
+      data: {
+        q,
+        from,
+        to,
+        appKey:appid,
+        salt,
+        sign
+      },
+      success(res) {
+        console.log(res)
+        if (res.data && res.data.translation) {
+          resolve(res.data)
+        } else {
+          reject({ status: 'error', msg: '翻译失败' })
+          wx.showToast({
+            title: '翻译失败',
+            icon: 'none',
+            duration: 3000
+          })
+        }
+      },
+      fail() {
+        reject({ status: 'error', msg: '翻译失败' })
+        wx.showToast({
+          title: '网络异常',
+          icon: 'none',
+          duration: 3000
+        })
+      }
+    })
+  })
+}
+module.exports.translate = translate
