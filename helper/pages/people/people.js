3fb88c4989c3e2f21a2d406fc294ed3097b10c26
licb <545360100@qq.com>
2022-08-06 14:27:58 +0800

    feat: hidden add chicken leg

--- helper/pages/people/people.js
+++ helper/pages/people/people.js
@@ -22,14 +22,14 @@ Page({
     app.globalData.userInfo = this.data.userInfo;
     console.log(app.globalData.userInfo)
   },
+  // 加鸡腿功能暂时没办法使用
   onTapHelp:function(){
     console.log(this.data.canTap)
     if (this.data.canTap !== true) {
       return
     }
     this.data.canTap=false
-    setTimeout(()=> { this.data.canTap=true},1000)
-
+    setTimeout(()=> {this.data.canTap=true},1000)
     wx.navigateToMiniProgram({
       appId: 'wx18a2ac992306a5a4',
       path: 'pages/apps/largess/detail?id=S5RkXLhilUU%3D'
